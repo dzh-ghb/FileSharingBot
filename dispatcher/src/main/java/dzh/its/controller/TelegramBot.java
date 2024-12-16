903df83693d81226aa3706af2b21b99886b97f69
dzh <d.snitchester@gmail.com>
2024-12-16 23:24:02 +0300

    adding logging to dispatcher, connecting the lombok library, adding the ability to send a reply message to chat with bot

--- dispatcher/src/main/java/dzh/its/controller/TelegramBot.java
+++ dispatcher/src/main/java/dzh/its/controller/TelegramBot.java
@@ -1,14 +1,18 @@
 package dzh.its.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component //аннотация, чтобы Spring создал бин и поместил его в контекст
+@Log4j //аннотация, автоматически под капотом добавляющая функциональность (logger), использование Lombok
 public class TelegramBot extends TelegramLongPollingBot {
-    @Value("${bot.name}")
+    @Value("${bot.name}") //аннотация, устанавливающая значение переменной ниже из файла со свойствами
     private String botName;
     @Value("${bot.token}")
     private String botToken;
@@ -26,6 +30,21 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
+
+        SendMessage response = new SendMessage();
+        response.setChatId(originalMessage.getChatId());
+        response.setText("DZHITS");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
