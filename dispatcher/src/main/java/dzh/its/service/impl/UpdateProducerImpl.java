1bdb0d63f4722d18173124e3e15ce140904a08e3
dzh <d.snitchester@gmail.com>
2024-12-25 19:09:55 +0300

    deploying RabbitMQ in Docker, integrating the microservice with the message broker, configuring information transfer to the message broker depending on the type of messages

--- dispatcher/src/main/java/dzh/its/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/dzh/its/service/impl/UpdateProducerImpl.java
@@ -3,6 +3,7 @@ package dzh.its.service.impl;
 import dzh.its.service.UpdateProducer;
 import lombok.extern.log4j.Log4j;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -10,8 +11,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Service
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate; //bean из зависимости-стартера spring-boot-starter-amqp
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
