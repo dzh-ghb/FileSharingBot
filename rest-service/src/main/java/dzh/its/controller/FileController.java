7f6db11f97d47b56ef6b73bfecf01ed9eff42b2c
dzh <d.snitchester@gmail.com>
2025-01-23 21:50:31 +0300

    changing the logic of file processing: creating a file for downloading from an array of bytes without using temporary files

--- rest-service/src/main/java/dzh/its/controller/FileController.java
+++ rest-service/src/main/java/dzh/its/controller/FileController.java
@@ -5,13 +5,15 @@ import dzh.its.entity.AppPhoto;
 import dzh.its.entity.BinaryContent;
 import dzh.its.service.FileService;
 import lombok.extern.log4j.Log4j;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Log4j
 //установка общей для всех методов в контроллере части uri-пути (во избежание дублирования кода)
@@ -27,44 +29,52 @@ public class FileController { //для обработки входящих http-
 
     //method - тип запроса, который будет отрабатываться, value - установка идентификатора ресурса для uri-пути
     @RequestMapping(method = RequestMethod.GET, value = "/get-doc")
-    public ResponseEntity<?> getDoc(@RequestParam("id") String id) { //получение документа
-        //ResponseEntity<?> - Spring-класс для удобной сборки http-ответа, @RequestParam("id") - описывает ожидаемые параметры входящего GET-запроса
+    public void getDoc(@RequestParam("id") String id, HttpServletResponse response) { //получение документа, @RequestParam("id") - описывает ожидаемые параметры входящего GET-запроса
+        //вместо формирования ответа через ResponseEntity<?> (Spring-класс для удобной сборки http-ответа), ответ формируется вручную через перехваченный объект HttpServletResponse
         //TODO: добавить ControllerAdvice для формирования Bad Request (для обработки внутренних ошибок и формирования детализированных ответов юзеру)
         AppDocument doc = fileService.getDocument(id); //получение из FileService объекта документа по идентификатору
-        if (doc == null) {
-            return ResponseEntity.badRequest().build(); //возврат 400-го ответа (Bad Request) об ошибке в запросе (если документа нет)
+        if (doc == null) { //если объект не был найден
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); //возврат 400-го ответа (Bad Request) - считаем, что юзер прислал некорректный id
+            return;
         }
+        //иначе, ручное формирование успешного ответа
+        response.setContentType(String.valueOf(MediaType.parseMediaType(doc.getMimeType()))); //формат файла, в который поток байтов будет преобразован после скачивания
+        response.setHeader("Content-disposition", "attachment; filename=" + doc.getDocName()); //указывает браузеру, что файл необходимо скачать
+        //(attachment - скачивание файла) и присвоить имя (filename - имя файла) - без указания заголовка файл откроется в браузере без скачивания
+        response.setStatus(HttpServletResponse.SC_OK); //200-й ответ (OK)
 
         BinaryContent binaryContent = doc.getBinaryContent(); //получение BinaryContent объекта из полученного документа
-        FileSystemResource fileSystemResource = fileService.getFileSystemResource(binaryContent); //формирование объекта, который можно отправить в теле ответа юзеру
-        if (fileSystemResource == null) {
-            return ResponseEntity.internalServerError().build(); //возврат 500-го ответа (Internal Server Error) об ошибке на стороне сервера
-            //(например, если документ найден в БД, НО его не получилось вернуть)
+        try {
+            ServletOutputStream out = response.getOutputStream();
+            out.write(binaryContent.getFileAsArrayOfBytes()); //запись массива байтов с контентом напрямую в поток вывода
+            //(не нужно хранить временный файл в постоянной памяти приложения)
+            out.close(); //закрытие потока вывода
+        } catch (IOException e) { //если возникнет ошибка
+            log.error(e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); //изменение статуса ответа на 500-ый (Internal Server Error)
         }
-        return ResponseEntity.ok() //возврат 200-го ответа (OK) при успешном создании объекта FileSystemResource
-                .contentType(MediaType.parseMediaType(doc.getMimeType())) //добавление к ответу заголовка с форматом контента
-                //(чтобы браузер создал из потока байтов файл с нужным расширением)
-                .header("Content-disposition", "attachment; filename=" + doc.getDocName()) //указывает браузеру, как воспринимать информацию:
-                //attachment - скачивание файла, filename - имя файла (без заголовка файл откроется в браузере без скачивания)
-                .body(fileSystemResource);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/get-photo")
-    public ResponseEntity<?> getPhoto(@RequestParam("id") String id) { //получение фото, метод идентичен getDoc()
+    public void getPhoto(@RequestParam("id") String id, HttpServletResponse response) { //получение фото, метод идентичен getDoc()
         //TODO: добавить ControllerAdvice для формирования badRequest
         AppPhoto photo = fileService.getPhoto(id);
         if (photo == null) {
-            return ResponseEntity.badRequest().build();
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
+        response.setContentType(String.valueOf(MediaType.IMAGE_JPEG)); //после скачивания поток байтов будет преобразован в файл формата JPEG
+        response.setHeader("Content-disposition", "attachment;");
+        response.setStatus(HttpServletResponse.SC_OK);
 
         BinaryContent binaryContent = photo.getBinaryContent();
-        FileSystemResource fileSystemResource = fileService.getFileSystemResource(binaryContent);
-        if (fileSystemResource == null) {
-            return ResponseEntity.internalServerError().build();
+        try {
+            ServletOutputStream out = response.getOutputStream();
+            out.write(binaryContent.getFileAsArrayOfBytes());
+            out.close();
+        } catch (IOException e) {
+            log.error(e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_JPEG) //все фото хранятся в формате JPEG
-                .header("Content-disposition", "attachment;") //для фото имя файла неизвестно
-                .body(fileSystemResource);
     }
 }
